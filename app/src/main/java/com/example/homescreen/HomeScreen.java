42cd1ef7c7c1d9b61eae3667d4302dc230aafa9e
Timvdperk <timvdperk82@gmail.com>
2019-05-01 17:42:51 +0200

    Nav bar works now on the homescreen only, problem is that the listview won't scroll and istn clickable. Probably a placement mistake in Homescreen.java

--- app/src/main/java/com/example/homescreen/HomeScreen.java
+++ app/src/main/java/com/example/homescreen/HomeScreen.java
@@ -2,19 +2,59 @@ package com.example.homescreen;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if ( id == R.id.myprofile)
+                {
+                    Toast.makeText(HomeScreen.this , "MyProfile", Toast.LENGTH_SHORT).show();
+                }
+                if ( id == R.id.mysettings)
+                {
+                    Toast.makeText(HomeScreen.this , "Settings", Toast.LENGTH_SHORT).show();
+                }
+                if ( id == R.id.myedit)
+                {
+                    Toast.makeText(HomeScreen.this , "Edit", Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
+
         String[] values = new String[]{
                 "Informatica  03-12-18", "Technische informatica  03-12-18", "Communicatie", "Communication and Multimedia Design",
                 "Creative Media and Game Technologies", "Informatica", "Technische informatica", "Communicatie", "Communication and Multimedia Design",
@@ -29,14 +69,14 @@ public class HomeScreen extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+
                 if(position==0){
                     Intent myIntent = new Intent(view.getContext(), InformaticaOpendag.class);
                     startActivityForResult(myIntent, 0 );
                 }
                 if (position==1){
-                Intent myIntent = new Intent(view.getContext(), TechnInform1.class);
-                startActivityForResult(myIntent,0);
+                    Intent myIntent = new Intent(view.getContext(), TechnInform1.class);
+                    startActivityForResult(myIntent,0);
                 }
                 if (position==2) {
                     Intent myIntent = new Intent(view.getContext(), TechnInform1.class);
@@ -94,18 +134,11 @@ public class HomeScreen extends AppCompatActivity {
             }
         });
 
-
-
-        }
-
-
-
-
-
-
-
     }
 
-    //http://www.worldbestlearningcenter.com/tips/Android-ListView-item-and-button-clickable.htm
-    //https://www.youtube.com/watch?v=UlUwi5EMspc
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
 
+}
