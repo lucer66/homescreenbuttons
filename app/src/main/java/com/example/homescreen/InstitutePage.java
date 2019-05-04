6a4851094ff69691570faec991843f4bff5bacbe
TimoPolderman <43845528+TimoPolderman@users.noreply.github.com>
2019-05-04 12:53:16 +0200

    institutepage

---
+++ app/src/main/java/com/example/homescreen/InstitutePage.java
@@ -0,0 +1,13 @@
+package com.example.homescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstitutePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_institute_page);
+    }
+}
